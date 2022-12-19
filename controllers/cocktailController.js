025847ba2bdfdc2527655e3358102f4b2981e4de
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-19 15:14:52 -0600

    Create cocktail controllers

---
+++ controllers/cocktailController.js
@@ -0,0 +1,24 @@
+const asyncHandler = require('express-async-handler');
+
+const getCocktails = asyncHandler(async (req, res) => {
+    const cocktails = await Cocktail.find({})
+    res.status(200).json(cocktails)
+})
+
+const createCocktail = asyncHandler(async (req,res) => {
+    const {name, specs} = req.body;
+
+    //Remember to add image too but not yet
+
+    if(!name || !specs) {
+        res.status(400)
+        throw new Error("Please fill in all fields")
+    }
+
+    const cocktail = await Cocktail.create({
+        name,
+        specs
+    })
+
+    res.status(201).json(cocktail)
+})
