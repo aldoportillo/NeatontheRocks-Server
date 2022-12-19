67e849f30cb1fda64b42f3cc92248cd465527322
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-19 14:57:00 -0600

    Add strict query rule

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 
 mongoose
+    .set("strictQuery", false)
     .connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(PORT, () => {
@@ -25,5 +26,5 @@ app.use(cors())
 //app.use(routes)
 
 app.get("/", (req, res) => {
-    res.send(<h1>Neat on the Rocks Server -- Please Leave</h1>)
+    res.send("<h1>Neat on the Rocks Server -- Please Leave</h1>")
 })
