6435495d5c7e31a310b005d32b73e9064c331014
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-19 15:35:56 -0600

    Export Router

--- routes/cocktailRoutes.js
+++ routes/cocktailRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express")
-const {createCocktail, getCocktails} = require('../controllers/cocktailController')
-
+const {createCocktail, getCocktails} = require("../controllers/cocktailController")
 const router = express.Router();
 
 router.post("/api/cocktail", createCocktail)
@@ -8,3 +7,5 @@ router.post("/api/cocktail", createCocktail)
 router.get("/api/cocktails", getCocktails)
 
 //router.get("/api/cocktails/:params", getCocktail)
+
+module.exports = router
