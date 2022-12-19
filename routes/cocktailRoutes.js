b9cf4ca808820bfe959696d8aff6be7420a96d6a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-19 15:14:40 -0600

    Create cocktail routes

---
+++ routes/cocktailRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const {createCocktail, getCocktails} = require('../controllers/cocktailController')
+
+const router = express.Router();
+
+router.post("/api/cocktail", createCocktail)
+
+router.get("/api/cocktails", getCocktails)
+
+//router.get("/api/cocktails/:params", getCocktail)
