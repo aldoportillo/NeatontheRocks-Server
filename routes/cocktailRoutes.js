4b22ca7bc2479d143307e18c93bc6514b421558f
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-20 01:08:04 -0600

    Add get cocktail route

--- routes/cocktailRoutes.js
+++ routes/cocktailRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express")
-const {createCocktail, getCocktails} = require("../controllers/cocktailController")
+const {createCocktail, getCocktails, getCocktail} = require("../controllers/cocktailController")
 const router = express.Router();
 
 router.post("/api/cocktail", createCocktail)
 
 router.get("/api/cocktails", getCocktails)
 
-//router.get("/api/cocktails/:params", getCocktail)
+router.get("/api/cocktails/:id", getCocktail)
 
 module.exports = router
