a0678fbfb144251904aa40272f7be46e2ca6148a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-22 19:43:53 -0600

    Create product routes

---
+++ routes/productRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const {createProduct, getProducts} = require("../controllers/productController")
+const router = express.Router();
+
+router.post("/api/product", createProduct)
+
+router.get("/api/products", getCocktails)
+
+module.exports = router
